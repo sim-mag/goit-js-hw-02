ed8124864f5a9e0f2365f57c310c3e0e23c286ad
sim-mag <79795891+sim-mag@users.noreply.github.com>
2024-09-08 11:25:16 +0200

    updated task 3

---
+++ js/task3.js
@@ -0,0 +1,11 @@
+// SPAM protection
+
+function checkForSpam(message){
+    let lowerCaseMessage = message.toLowerCase();
+    return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
+}
+
+console.log(checkForSpam("Latest sale offers")); // true
+console.log(checkForSpam("Get your free spam")); // true
+console.log(checkForSpam("Hello, how are you?")); // false
+console.log(checkForSpam("Amazing deal on shoes!")); // false
